5ff00bf5b3ca847d91af2cd88d8c9fe91af57347
lfantoniosi <felipe.antoniosi@gmail.com>
2023-11-12 01:44:11 -0800

    fix SCC, needs to compile disabling pinfilter FASTREAD
    fastread is required for hc-95 in turbo mode

--- fpga/src/pinfilter.v
+++ fpga/src/pinfilter.v
@@ -1,6 +1,7 @@
 // filter noise from GPIO pins
 // takes 2 cycles with enabled signal to stabilize a line
-// tri-states when not stable
+
+//`define FASTREAD
 
 module pinfilter(
     input clk,
@@ -10,30 +11,32 @@ module pinfilter(
     output dout
 );
 
-    logic [1:0] dpipe;
-
+    reg [1:0] dpipe;
     reg d;
-    
+    reg d2;
+
     always @(posedge clk or negedge reset_n) begin
 
     if (~reset_n) begin
 
-            d <= 1'b1;
             dpipe <= 2'b11;
 
         end else begin
 
             if (ena) begin
-                dpipe <= { dpipe[0], din };
-            end
 
-            d <= (dpipe[1:0] == 2'b00) ? 1'b0 : (dpipe[1:0] == 2'b11) ? 1'b1 : d; 
+                dpipe <= { dpipe[0], din };
+
+                d <= (dpipe[1:0] == 2'b00) ? 1'b0 : (dpipe[1:0] == 2'b11) ? 1'b1 : d;
+
+            end
+`ifdef FASTREAD
+            d <= (dpipe[1:0] == 2'b00) ? 1'b0 : (dpipe[1:0] == 2'b11) ? 1'b1 : d;
+`endif
 
         end
     end
 
-    assign dout = d;
-
-//    assign dout = (dpipe[1:0] == 2'b00) ? 1'b0 : (dpipe[1:0] == 2'b11) ? 1'b1 : d; 
+assign dout =  d;
 
 endmodule
