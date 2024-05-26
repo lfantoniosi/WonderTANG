339c7b05c2e7e6092691c992162e8973f7dd6443
lfantoniosi <felipe.antoniosi@gmail.com>
2024-05-26 02:53:10 -0700

    fix opll and some audio changes

--- fpga/src/pinfilter.v
+++ fpga/src/pinfilter.v
@@ -2,7 +2,10 @@
 // takes 2 cycles with enabled signal to stabilize a line
 
 
-module pinfilter(
+module pinfilter
+#(
+    parameter bit REGISTERED = 1'b0
+)(
     input clk,
     input reset_n,
     input din,
@@ -25,43 +28,14 @@ module pinfilter(
             if (ena) begin
 
                 dpipe <= { dpipe[0], din };
+
+                if (REGISTERED)
+                    dout <= (dpipe[1:0] == 2'b00) ? 1'b0 : (dpipe[1:0] == 2'b11) ? 1'b1 : dout;
 
             end
 
-            dout <= (dpipe[1:0] == 2'b00) ? 1'b0 : (dpipe[1:0] == 2'b11) ? 1'b1 : dout;
-
-        end
-    end
-
-endmodule
-
-module pinfilter2(
-    input clk,
-    input reset_n,
-    input din,
-    input ena,
-    output reg dout
-);
-
-    reg [1:0] dpipe;
-    reg d;
-
-    always @(posedge clk or negedge reset_n) begin
-
-    if (~reset_n) begin
-
-            dpipe <= 2'b11;
-            dout <= 1'b1;
-
-        end else begin
-
-            if (ena) begin
-
-                dpipe <= { dpipe[0], din };
-
+            if (~REGISTERED)
                 dout <= (dpipe[1:0] == 2'b00) ? 1'b0 : (dpipe[1:0] == 2'b11) ? 1'b1 : dout;
-
-            end
 
         end
     end
